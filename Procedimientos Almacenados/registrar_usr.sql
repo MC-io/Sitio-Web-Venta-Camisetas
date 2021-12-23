7e0207cdd44c11bea64be85b6b7e893082cf54cc
Moises <mcasaverde@unsa.edu.pe>
2021-12-23 11:50:38 -0500

    Updated SP registrar_usr

--- Procedimientos Almacenados/registrar_usr.sql
+++ Procedimientos Almacenados/registrar_usr.sql
@@ -3,19 +3,39 @@ use sin_nombre;
 DELIMITER //
 
 drop procedure if exists registrar_usr//
-create procedure registrar_usr(in _dni int, in _nombres varchar(30), in _prim_ape varchar(30),
-in _seg_ape varchar(30), in _pais varchar(30), in _fecha_nac date)
+create procedure registrar_usr(
+in _dni int,
+in _nombres varchar(30),
+in _prim_ape varchar(30),
+in _seg_ape varchar(30),
+in _pais varchar(30),
+in _fecha_nac date,
+in _email varchar(50),
+in _paswrd varchar(200))
 begin
 	
 	declare _id_pais int;
     declare _id_contacto int;
-    set _id_pais = (select p.ID
-    from paises p
-    where _pais = p.Nombre
-    );
-    insert into contacto values (null);
-    set _id_contacto = (select last_insert_id());
-    insert into usuarios values (_dni, _nombres, _prim_ape, _seg_ape,_id_pais,_fecha_nac,_id_contacto); 
-end  ;
+    declare EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
+        SELECT @error_string;
+        ROLLBACK;
+    END;
+    START TRANSACTION;
+		IF(SELECT COUNT(*) FROM usuarios WHERE DNI = _dni) = 0 THEN /* Si no existe ya */
+			IF(SELECT COUNT(*) FROM loginusr WHERE Email = _email) = 0 THEN  /* Y el nombre del pais existe */
+				set _id_pais = (select p.ID
+                from paises p
+                where _pais = p.Nombre
+                );
+                insert into contacto values (null);
+                set _id_contacto = (select last_insert_id());
+                insert into usuarios values (_dni, _nombres, _prim_ape, _seg_ape,_id_pais,_fecha_nac,_id_contacto);
+                insert into loginusr values (_dni, _email, _paswrd);
+            END IF;
+        END IF;
+    COMMIT;
+end ;
 // 
 DELIMITER ; 
---
+++ inserciones.sql
@@ -0,0 +1,51 @@
+
+use sin_nombre;
+
+# Insertando paises
+
+insert into paises
+values (null,'Peru','America');
+insert into paises
+values (null,'Chile','America');
+insert into paises
+values (null,'Colombia','America');
+insert into paises
+values (null,'Estados Unidos','America');
+insert into paises
+values (null,'Alemania','Europa');
+insert into paises
+values (null,'Francia','Europa');
+insert into paises
+values (null,'Italia','Europa');
+insert into paises
+values (null,'Mexico','America');
+insert into paises
+values (null,'Japon','Asia');
+insert into paises
+values (null,'Corea del Sur','Asia');
+insert into paises
+values (null,'Indonesia','Asia');
+insert into paises
+values (null,'Egipto','Africa');
+insert into paises
+values (null,'Australia','Oceania');
+insert into paises
+values (null,'Rusia','Europa');
+insert into paises
+values (null,'Reino Unido','Europa');
+
+# Insertando usuarios :D
+
+
+call registrar_usr(90438223,'Maria Alejandra','Cuellar','Quispe','Mexico','2000-11-23');
+call registrar_usr(18795457,'Ernesto Enrique','Perez','Acosta','Colombia','1999-10-02');
+call registrar_usr(18795457,'Zhong Xina','Ching','Cheng','China','2003-03-09');
+call registrar_usr(90231873,'Ander Patrick','Smith','Ape','Estados Unidos','1986-08-12');
+call registrar_usr(90231873,'Louis Pierre','Macron','Gaulle','Francia','1997-01-17');
+call registrar_usr(90231873,'Kumar Jhero','Khan','Sharma','Estados Unidos','1986-08-12');
+
+
+
+
+
+
