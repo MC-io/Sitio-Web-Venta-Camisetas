ba60cb8cdb77ad593b3573ff629f19ce7f6f526d
Ruzo <63430135+Rushell-max@users.noreply.github.com>
2021-12-23 14:08:33 -0500

    UPDATE SP

--- Procedimientos Almacenados/CDSP.sql
+++ Procedimientos Almacenados/CDSP.sql
@@ -1,5 +1,83 @@
 USE sin_nombre;
-/** AQUI CIUDAD, DIRECCION, SEDE Y PRODUCTO **/
+
+/************ PAIS **************************/
+/*
+CREATE TABLE Paises(
+	ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    Nombre VARCHAR(30),
+    Continente ENUM('África', 'Asia', 'América', 'Antártida', 'Europa', 'Oceanía')
+);
+*/
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS insertar_Pais; //
+CREATE PROCEDURE insertar_Pais(
+	IN _Nombre VARCHAR(30), 
+    IN _Continente ENUM('África', 'Asia', 'América', 'Antártida', 'Europa', 'Oceanía'))
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
+        SELECT @error_string;
+        ROLLBACK;
+    END;
+    START TRANSACTION;
+		IF(SELECT COUNT(*) FROM Paises WHERE Nombre = _Nombre) = 0 THEN /* Si no existe ya */
+			INSERT INTO Paises(Nombre, Continente) VALUES (_Nombre, _Continente); /* E insertamos */
+        END IF;
+    COMMIT;
+END;
+//
+DELIMITER;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS eliminar_Pais; //
+CREATE PROCEDURE eliminar_Pais(
+	IN _Nombre VARCHAR(30))
+BEGIN
+	DECLARE _ID_Pais INTEGER;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlsate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
+        SELECT @error_string;
+        ROLLBACK;
+    END;
+    START TRANSACTION;
+		IF(SELECT COUNT(*) FROM Paises WHERE Nombre = _Nombre) = 1 THEN  /* Si existe una ciudad con ese nombre */
+			SET _ID_Pais = (SELECT ID FROM Paises WHERE Nombre = _Nombre); 
+			IF(SELECT COUNT(*) FROM Ciudades WHERE ID_Pais = _ID_Pais) = 0 THEN /*Si no hay ciudades en el pais */
+				DELETE FROM Paises WHERE ID = _ID_Pais;
+			END IF;
+        END IF;
+    COMMIT;
+END;
+//
+DELIMITER ;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS actualizar_Pais_Nombre; //
+CREATE PROCEDURE actualizar_Pais_Nombre(
+	IN _Nombre VARCHAR(30), 
+    IN _NewNombre VARCHAR(30))
+BEGIN
+	DECLARE _ID_Pais INTEGER;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlsate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
+        SELECT @error_string;
+        ROLLBACK;
+    END;
+    START TRANSACTION;
+		IF(SELECT COUNT(*) FROM Paises WHERE Nombre = _Nombre) = 1 THEN  /* Si existe un pais con ese nombre */
+            SET _ID_Pais = (SELECT ID FROM Paises WHERE Nombre = _Nombre);  /* Sacamos su ID */
+            UPDATE Paises SET Nombre = _NewNombre WHERE ID = _ID_Pais;  /* Actualizamoa */
+        END IF;
+    COMMIT;
+END;
+//
+DELIMITER ;
+
+/************ -- PAIS -- **************************/
 
 /************ CIUDAD **************************/
 
@@ -164,25 +242,33 @@ DELIMITER ;
 
 /*************** -- DIRECCION -- *********************/
 
-/***************  SEDE  **************************/
+/***************  EMAIL  **************************/
 /*
-CREATE TABLE Sedes(
-	ID INTEGER PRIMARY KEY,
-    Nombre VARCHAR(30),
-    Estado ENUM('Activo','No Activo'),
-    ID_Ciudad INTEGER
+CREATE TABLE IF NOT EXISTS Emails(
+	ID INTEGER PRIMARY KEY AUTO_INCREMENT,
+    Email VARCHAR(50),
+    Interesado BOOL,
+    ID_Contacto INTEGER
+);
+*/
+
+/***************  -- EMAIL --  **************************/
+
+/***************  TELEFONO  **************************/
+/*
+CREATE TABLE IF NOT EXISTS Telefonos(
+    Telefono INTEGER,
+    ID_Contacto INTEGER,
+    PRIMARY KEY(ID_Contacto, Telefono)
 );
 */
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS insertar_Sede; //
-CREATE PROCEDURE insertar_Sede(
-	IN _Nombre VARCHAR(30), 
-    IN _Estado ENUM('Activo','No Activo'),
-    In _NombreCiudad VARCHAR(30))
+DROP PROCEDURE IF EXISTS insertar_Telefono; //
+CREATE PROCEDURE insertar_Telefono(
+	IN _Telefono INTEGER, 
+    IN _ID_Contacto INTEGER)
 BEGIN
-	DECLARE _ID_Ciudad INTEGER;
-    DECLARE _ID INTEGER;
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
 		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
@@ -190,90 +276,16 @@ BEGIN
         ROLLBACK;
     END;
     START TRANSACTION;
-		IF(SELECT COUNT(*) FROM Sedes WHERE Nombre = _Nombre) = 0 THEN /* Si no existe la sede ya */
-			IF(SELECT COUNT(*) FROM Ciudades WHERE Nombre = _NombreCiudad) = 1 THEN /* Si la ciudad existe */
-				SET _ID_Ciudad = (SELECT ID FROM Ciudades WHERE Nombre = _NombreCiudad); /* Sacamos el ID de la ciudad */
-				INSERT INTO Contacto VALUES (NULL);
-				SET _ID = (SELECT last_insert_id());
-				INSERT INTO Sedes(ID, Nombre, Estado, ID_Ciudad) VALUES (_ID, _Nombre, _Estado, _ID_Ciudad); /* E insertamos */
+		IF(SELECT COUNT(*) FROM Contacto WHERE ID = _ID_Contacto) = 1 THEN /* Si existe */
+			IF(SELECT COUNT(*) FROM Telefonos WHERE ID = _ID_Contacto AND _Telefono = Telefono) = 0 THEN  /* Si no existe ya*/
+					INSERT INTO Telefonos(Telefono, ID_Contacto) VALUES (_Telefono, _ID_Contacto);
 			END IF;
         END IF;
     COMMIT;
 END;
 //
-
-DELIMITER //
-DROP PROCEDURE IF EXISTS actualizar_Sede_Nombre; //
-CREATE PROCEDURE actualizar_Sede_Nombre(
-	IN _Nombre VARCHAR(30), 
-    IN _NewNombre VARCHAR(30))
-BEGIN
-	DECLARE _ID INTEGER;
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-		GET DIAGNOSTICS CONDITION 1 @sqlsate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
-        SELECT @error_string;
-        ROLLBACK;
-    END;
-    START TRANSACTION;
-		IF(SELECT COUNT(*) FROM Sedes WHERE Nombre = _Nombre) = 1 THEN  /* Si existe una sede con ese nombre */
-            SET _ID = (SELECT ID FROM Sedes WHERE Nombre = _Nombre);  /* Sacamos su ID */
-            UPDATE Sedes SET Nombre = _NewNombre WHERE ID = _ID;  /* Actualizamoa */
-        END IF;
-    COMMIT;
-END;
-//
-DELIMITER ;
-
-DELIMITER //
-DROP PROCEDURE IF EXISTS cambiarEstadoSede; //
-CREATE PROCEDURE cambiarEstadoSede(
-	IN _Nombre VARCHAR(30))
-BEGIN
-    DECLARE _ID INTEGER;
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
-        SELECT @error_string;
-        ROLLBACK;
-    END;
-    START TRANSACTION;
-		IF(SELECT COUNT(*) FROM Sedes WHERE Nombre = _Nombre) = 1 THEN /* Si la sede existe */
-			SET _ID = (SELECT ID FROM Sedes WHERE Nombre = _Nombre); /* Sacamos su ID */
-            IF(SELECT Estado FROM Sedes WHERE ID = _ID) = "Activo" THEN 
-				UPDATE Sedes SET Estado = "No Activo" WHERE ID = _ID;
-			ELSEIF (SELECT Estado FROM Sedes WHERE ID = _ID) = "No Activo" THEN
-				UPDATE Sedes SET Estado = "Activo" WHERE ID = _ID;
-            END IF;
-        END IF;
-    COMMIT;
-END;
-//
-
-DELIMITER //
-DROP PROCEDURE IF EXISTS eliminar_Sede; //
-CREATE PROCEDURE eliminar_Sede(
-	IN _Nombre VARCHAR(30))
-BEGIN
-	DECLARE _ID INTEGER;
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    BEGIN
-		GET DIAGNOSTICS CONDITION 1 @sqlsate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
-        SELECT @error_string;
-        ROLLBACK;
-    END;
-    START TRANSACTION;
-		IF(SELECT COUNT(*) FROM Sedes WHERE Nombre = _Nombre) = 1 THEN  /* Si existe una sede con ese nombre */
-			SET _ID = (SELECT ID FROM Sedes WHERE Nombre = _Nombre); 
-			IF(SELECT COUNT(*) FROM Productos_Sedes WHERE ID_Sede = _ID) = 0 THEN /*Si no hay productos en la sede */
-				DELETE FROM Sedes WHERE ID = _ID;
-			END IF;
-        END IF;
-    COMMIT;
-END;
-//
-DELIMITER ;
-/***************  -- SEDE --  **************************/
+DELIMITER;
+/***************  -- TELEFONO --  **************************/
 
 /************ PROVEEDOR **************************/
 /*
@@ -353,15 +365,7 @@ END;
 DELIMITER ;
 /************ -- PROVEEDOR -- **************************/
 
-/***************  PRODUCTO-SEDE // PROVEEDORES - PRODUCTOS **************************/
-/*
-CREATE TABLE IF NOT EXISTS Productos_Sedes(
-	ID_Producto INTEGER,
-    ID_Sede INTEGER,
-    Stock INTEGER,
-    PRIMARY KEY(ID_Producto, ID_Sede)
-);
-*/
+/***************  PRODUCTO // PROVEEDORES - PRODUCTOS **************************/
 /*
 CREATE TABLE IF NOT EXISTS Proveedores_Productos(
 	ID_Proveedor INTEGER,
@@ -371,13 +375,14 @@ CREATE TABLE IF NOT EXISTS Proveedores_Productos(
 */
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS insertar_Producto; //
-CREATE PROCEDURE insertar_Producto(
+DROP PROCEDURE IF EXISTS crear_Producto; //
+CREATE PROCEDURE crear_Producto(
 	IN _Nombre VARCHAR(30), 
     IN _IMG BLOB,
     In _Precio DECIMAL,
     IN _Talla ENUM('XS', 'S', 'M', 'L', 'XL', 'XXL'),
     IN _Para ENUM('F', 'M'),
+    IN _Stock INTEGER,
     IN _NombreCategoria VARCHAR(30),
     IN _NombreProveedor VARCHAR(30))
 BEGIN
@@ -396,7 +401,7 @@ BEGIN
 				IF(SELECT COUNT(*) FROM Proveedores WHERE Nombre = _NombreProveedor) = 1 THEN /* Si el proveedor existe */
 					SET _ID_Categoria = (SELECT ID FROM Categorias WHERE Nombre = _NombreCategoria); /* Sacamos el ID de la categoria */
 					SET _ID_Proveedor = (SELECT ID FROM Proveedores WHERE Nombre = _NombreProveedor); /* Sacamos el ID del proveedor  */
-					INSERT INTO Productos(Nombre, IMG, Precio, Talla, Para, ID_Categoria) VALUES (_Nombre, _IMG, _Precio, _Talla, _Para, _ID_Categoria); /* E insertamos */
+					INSERT INTO Productos(Nombre, IMG, Precio, Talla, Para, Stock, ID_Categoria) VALUES (_Nombre, _IMG, _Precio, _Talla, _Para, _Stock , _ID_Categoria); /* E insertamos */
 					SET _ID = (SELECT last_insert_id()); /* Obtenemos su ID */
                     INSERT INTO Proveedores_Productos(ID_Proveedor, ID_Producto) VALUES (_ID_Proveedor, _ID); /* Insertamos en proveedores */
                 END IF;
@@ -405,7 +410,7 @@ BEGIN
     COMMIT;
 END;
 //
-DELIMITER;
+DELIMITER; 
 
 DELIMITER //
 DROP PROCEDURE IF EXISTS nuevo_ProveedorProducto; //
@@ -435,14 +440,12 @@ END;
 DELIMITER;
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS insertar_ProductoSede; //
-CREATE PROCEDURE insertar_ProductoSede(
-	IN _Nombre VARCHAR(30), 
-    IN _NombreSede VARCHAR(30),
+DROP PROCEDURE IF EXISTS agregar_ProductoStock; //
+CREATE PROCEDURE agregar_ProductoStock(
+	IN _Nombre VARCHAR(30),
     IN _PlusStock INTEGER
     )
 BEGIN
-    DECLARE _ID_Sede INTEGER;
     DECLARE _ID_Producto INTEGER;
     DECLARE _Stock INTEGER;
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
@@ -452,31 +455,21 @@ BEGIN
         ROLLBACK;
     END;
     START TRANSACTION;
-		IF(SELECT COUNT(*) FROM Sedes WHERE Nombre = _NombreSede) = 1 THEN /* Si la sede existe */
-			SET _ID_Sede = (SELECT ID FROM Sedes WHERE Nombre = _NombreSede); /* Sacamos el ID de la sede */
-            IF(SELECT COUNT(*) FROM Productos WHERE Nombre = _Nombre) = 1 THEN /* Si el producto ya existe */
-				SET _ID_Producto = (SELECT ID FROM Productos WHERE Nombre = _Nombre); /* Obtenemos su ID */
-                /** Si el producto no tiene stock en tal sede entonces insertamos **/
-				IF(SELECT COUNT(*) FROM  Productos_Sedes PS WHERE PS.ID_Producto = _ID_Producto AND PS.ID_Sede = _ID_Sede ) = 0 THEN 
-					INSERT INTO Productos_Sedes(ID_Producto, ID_Sede, Stock) VALUES(_ID_Producto, _ID_Sede, _PlusStock);
-				ELSE /** Si el producto si tiene stock **/
-					SET _Stock = (SELECT Stock FROM Productos_Sedes WHERE ID_Producto = _ID_Producto AND ID_Sede = _ID_Sede);  
-					UPDATE Productos_Sedes SET Stock = _Stock + _PlusStock WHERE ID_Producto = _ID_Producto AND ID_Sede = _ID_Sede;
-				END IF;
-			END IF;
+		IF(SELECT COUNT(*) FROM Productos WHERE Nombre = _Nombre) = 1 THEN /* Si el producto existe */
+			SET _ID_Producto = (SELECT ID FROM Productos WHERE Nombre = _Nombre); /* Obtenemos su ID */
+			SET _Stock = (SELECT Stock FROM Productos WHERE ID_Producto = _ID_Producto);  
+            UPDATE Productos SET Stock = _Stock + _PlusStock WHERE ID_Producto = _ID_Producto;
         END IF;
     COMMIT;
 END;
 //
 
 DELIMITER //
-DROP PROCEDURE IF EXISTS eliminar_ProductoSede; //
-CREATE PROCEDURE eliminar_ProductoSede(
-	IN _NombreProducto VARCHAR(30), 
-    IN _NombreSede VARCHAR(30),
-    IN _Cantidad INTEGER)
+DROP PROCEDURE IF EXISTS reducir_ProductoStock; //
+CREATE PROCEDURE reducir_ProductoStock(
+	IN _Nombre VARCHAR(30), 
+    IN _MoinsStock INTEGER)
 BEGIN
-    DECLARE _ID_Sede INTEGER;
     DECLARE _ID_Producto INTEGER;
     DECLARE _Stock INTEGER;
     DECLARE EXIT HANDLER FOR SQLEXCEPTION
@@ -486,21 +479,183 @@ BEGIN
         ROLLBACK;
     END;
     START TRANSACTION;
-		IF(SELECT COUNT(*) FROM Sedes WHERE Nombre = _NombreSede) = 1 THEN /* Si la sede existe */
-			SET _ID_Sede = (SELECT ID FROM Sedes WHERE Nombre = _NombreSede); /* Sacamos el ID de la sede */
-            IF(SELECT COUNT(*) FROM Productos WHERE Nombre = _NombreProducto) = 1 THEN /* Si el producto ya existe */
-				SET _ID_Producto = (SELECT ID FROM Productos WHERE Nombre = _Nombre); /* Obtenemos su ID */
-				IF(SELECT COUNT(*) FROM  Productos_Sedes PS WHERE PS.ID_Producto = _ID_Producto AND PS.ID_Sede = _ID_Sede ) = 1 THEN
-					SET _Stock = (SELECT Stock FROM Productos_Sedes WHERE ID_Producto = _ID_Producto AND ID_Sede = _ID_Sede);  
-                    IF(_Cantidad > _Stock) THEN 
-						SET _Cantidad = _Stock;
-                    END IF;
-                    UPDATE Productos_Sedes SET Stock = _Stock - _Cantidad WHERE ID_Producto = _ID_Producto AND ID_Sede = _ID_Sede;
-				END IF;
+		IF(SELECT COUNT(*) FROM Productos WHERE Nombre = _Nombre) = 1 THEN /* Si el producto existe */
+			SET _ID_Producto = (SELECT ID FROM Productos WHERE Nombre = _Nombre); /* Obtenemos su ID */
+			SET _Stock = (SELECT Stock FROM Productos WHERE ID_Producto = _ID_Producto);  
+			IF(_MoinsStock > _Stock) THEN 
+				SET _MoinsStock = _Stock;
 			END IF;
+			UPDATE Productos_Sedes SET Stock = _Stock - _MoinsStock WHERE ID_Producto = _ID_Producto;
         END IF;
     COMMIT;
 END;
 //
 
 /***************  -- PRODUCTO-SEDE // PROVEEDORES - PRODUCTOS -- **************************/
+
+/***************  CARRITO DE COMPRAS **************************/
+/*
+CREATE TABLE IF NOT EXISTS Productos_Carrito(
+	ID_Producto INTEGER,
+    ID_Carrito INTEGER,
+    Cantidad INTEGER,
+    PRIMARY KEY(ID_Producto, ID_Carrito)
+);
+
+CREATE TABLE IF NOT EXISTS CarritoCompras(
+	ID INTEGER PRIMARY KEY
+);
+
+*/
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS insertar_ProductoCarrito; // 
+CREATE PROCEDURE insertar_ProductoCarrito(
+	IN _NombreProducto VARCHAR(30),
+    IN _IDCarrito INTEGER,
+    IN _Cantidad INTEGER)
+BEGIN
+	DECLARE _ID_Producto INTEGER;
+    DECLARE _Stock INTEGER;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
+        SELECT @error_string;
+        ROLLBACK;
+    END;
+    START TRANSACTION;
+		IF(SELECT COUNT(*) FROM Productos WHERE Nombre = _NombreProducto) = 1 THEN /* Si el producto existe */
+			IF(SELECT COUNT(*) FROM CarritoCompras WHERE ID = _IDCarrito) = 1 THEN /* Si el carrito existe */
+				SET _ID_Producto = (SELECT ID FROM Productos WHERE Nombre = _NombreProducto);
+                SET _Stock = (SELECT Stock FROM Productos WHERE ID = _ID_Producto);
+                IF(_Cantidad > _Stock) THEN
+					SET _Cantidad = _Stock;
+				END IF;
+                INSERT INTO Productos_Carrito(ID_Producto, ID_Carrito, Cantidad) VALUES (_ID_Producto, _IDCarrito, _Cantidad);
+                UPDATE CarritoCompras SET Total = Total(_IDCarrito) WHERE ID = _IDCarrito;
+			END IF;
+		END IF;
+    COMMIT;
+END;
+//
+DELIMITER;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS eliminar_ProductoCarrito; //
+CREATE PROCEDURE eliminar_ProductoCarrito(
+	IN _NombreProducto VARCHAR(30), 
+    IN _IDCarrito INTEGER,
+    IN _CantidadQuitar INTEGER)
+BEGIN
+    DECLARE _ID_Producto INTEGER;
+    DECLARE _Cantidad INTEGER;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
+        SELECT @error_string;
+        ROLLBACK;
+    END;
+    START TRANSACTION;
+		IF(SELECT COUNT(*) FROM Productos WHERE Nombre = _NombreProducto) = 1 THEN /* Si el producto existe */
+            IF(SELECT COUNT(*) FROM CarritoCompras WHERE ID = _IDCarrito) = 1 THEN /* Si el carrito existe */
+				SET _ID_Producto = (SELECT ID FROM Productos WHERE Nombre = _NombreProducto); /* Sacamos el ID del producto */
+				IF(SELECT COUNT(*) FROM  Productos_Carrito WHERE ID_Producto = _ID_Producto AND ID_Carrito = _IDCarrito ) = 1 THEN
+					SET _Cantidad = (SELECT Cantidad FROM Productos_Carrito WHERE ID_Producto = _ID_Producto AND ID_Carrito = _IDCarrito );  
+                    IF(_CantidadQuitar > _Cantidad) THEN 
+						SET _CantidadQuitar = _Cantidad;
+                    END IF;
+                    UPDATE Productos_Carrito SET Cantidad = _Cantidad - _CantidadQuitar WHERE ID_Producto = _ID_Producto AND ID_Carrito = _IDCarrito;
+					UPDATE CarritoCompras SET Total = Total(_IDCarrito) WHERE ID = _IDCarrito;
+                END IF;
+			END IF;
+        END IF;
+    COMMIT;
+END;
+//
+
+/***************  -- CARRITO DE COMPRAS -- **************************/
+
+/***************  PEDIDOS **************************/
+/*
+CREATE TABLE IF NOT EXISTS Pedidos(
+	ID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    Fecha DATETIME, 
+    Estado ENUM('Pendiente', 'Procesando', 'En Transporte', 'Completo', 'Devuelto', 'Cancelado') NOT NULL,
+    DNI_Usuario INTEGER NOT NULL,
+    ID_Direccion INTEGER NOT NULL,
+    Total DECIMAL DEFAULT 0.00 NOT NULL,
+	ID_Cupon INTEGER DEFAULT NULL
+);
+*/
+
+/*
+CREATE TABLE IF NOT EXISTS Productos(
+	ID INTEGER AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(30),
+    IMG BLOB,
+    Precio DECIMAL(16,2),
+    Talla ENUM('XS', 'S', 'M', 'L', 'XL', 'XXL'),
+    Para ENUM('F', 'M'),
+    Stock INTEGER DEFAULT 0,
+    ID_Categoria INTEGER
+);
+
+*/
+DELIMITER //
+DROP FUNCTION IF EXISTS Total; //
+CREATE FUNCTION Total(
+	DNI_Usuario
+) RETURNS INT DETERMINISTIC
+BEGIN
+	DECLARE _Total;
+    SET _Total = (SELECT SUM(P.Precio*PC.Cantidad) FROM Productos_Carrito PC, Productos P WHERE PC.ID_Carrito = DNI_Usuario AND PC.ID_Producto = P.ID) 
+END;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS crear_Pedido; //
+CREATE PROCEDURE crear_Pedido(
+    IN _DNI_Usuario INTEGER,
+    IN _ID_Direccion INTEGER,
+    In _ID_Cupon INTEGER)
+BEGIN
+    DECLARE _Total INTEGER;
+	DECLARE _Fecha DATETIME;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
+        SELECT @error_string;
+        ROLLBACK;
+    END;
+    START TRANSACTION;
+		SET _Fecha = NOW();
+        SET _Total = Total(_DNI_Usuario);
+        IF (_Total > 0) THEN
+			INSERT INTO Pedidos(Fecha, Estado, DNI_Usuario, ID_Direccion, Total, ID_Cupon) VALUES (_Fecha, "Pendiente", _DNI_Usuario, _ID_Direccion, _Total, _ID_Cupon);
+		END IF;
+    COMMIT;
+END;
+//
+DELIMITER;
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS actualizar_Pedido; //
+CREATE PROCEDURE actualizar_Pedido(
+    IN _ID INTEGER, 
+    IN _Estado ENUM('Pendiente', 'Procesando', 'En Transporte', 'Completo', 'Devuelto', 'Cancelado'))
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+		GET DIAGNOSTICS CONDITION 1 @sqlstate = RETURNED_SQLSTATE, @error_string = MESSAGE_TEXT;
+        SELECT @error_string;
+        ROLLBACK;
+    END;
+    START TRANSACTION;
+		IF(SELECT COUNT(*) FROM Pedidos WHERE ID = _ID) = 1 THEN /* Si el pedido existe */
+			UPDATE Pedidos SET Estado = _Estado WHERE ID = _ID;
+        END IF;
+    COMMIT;
+END;
+//
+
+
+/***************  -- PEDIDOS -- **************************/
