3f0401d0df33919d1e0188c747e8b672df363984
Moises <mcasaverde@unsa.edu.pe>
2021-12-24 15:39:51 -0500

    User Report SP

---
+++ Procedimientos Almacenados/search_usrs_by_filters.sql
@@ -0,0 +1,16 @@
+use sin_nombre;
+
+DELIMITER //
+
+drop procedure if exists search_usrs_by_filters//
+create procedure search_usrs_by_filters(in _nombre varchar(100), in _pais varchar(30), in edad_min integer, in edad_max integer)
+begin 
+	select u.DNI, u.Nombres, u.PrimerApellido, u.SegundoApellido, p.Nombre, u.FechaNac
+    from usuarios u
+    inner join paises p
+    on p.ID = u.Pais
+    where concat(u.Nombres,' ',u.PrimerApellido,' ',u.SegundoApellido) like concat("%",_nombre,'%') 
+    and p.Nombre like concat('%',_pais,'%') 
+end ;
+//
+DELIMITER ;
