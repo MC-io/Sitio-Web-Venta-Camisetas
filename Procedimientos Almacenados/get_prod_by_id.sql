d0787703cd1484ac51f422fefa11842c59cba601
Moises <mcasaverde@unsa.edu.pe>
2021-12-26 18:07:58 -0500

    sps

---
+++ Procedimientos Almacenados/get_prod_by_id.sql
@@ -0,0 +1,14 @@
+use sin_nombre;
+
+DELIMITER //
+
+drop procedure if exists get_prod_by_id//
+create procedure get_prod_by_id(in _id integer)
+begin 
+	select p.ID, p.Nombre, p.IMG, p.Precio, p.Talla, p.Para, p.Stock, c.Nombre 'Categoria'
+    from productos p
+    inner join categorias c
+    on p.ID_Categoria = c.ID;
+end ;
+//
+DELIMITER ; 
